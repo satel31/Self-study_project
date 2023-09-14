eab939536af2a5df067e98a1fbdc8fcf5598d1b1
satel31 <blokhnina.tatiana@yandex.ru>
2023-09-15 01:18:04 +0300

    Added urls for test and material

--- apps/learning/urls.py
+++ apps/learning/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
+from apps.learning.views.material import MaterialCreateAPIView, MaterialListAPIView, MaterialDetailAPIView, \
+    MaterialUpdateAPIView, MaterialDeleteAPIView
 from apps.learning.views.section import SectionCreateAPIView, SectionListAPIView, SectionDetailAPIView, \
     SectionUpdateAPIView, SectionDeleteAPIView
+from apps.learning.views.test import TestCreateAPIView, TestListAPIView, TestDetailAPIView, TestUpdateAPIView, \
+    TestDeleteAPIView
 
 app_name = 'learning'
 
@@ -13,4 +17,17 @@ urlpatterns = [
     path('sections/update/<int:pk>/', SectionUpdateAPIView.as_view(), name='section_update'),
     path('sections/delete/<int:pk>/', SectionDeleteAPIView.as_view(), name='section_delete'),
 
+    # material
+    path('add_material/', MaterialCreateAPIView.as_view(), name='add_material'),
+    path('materials/', MaterialListAPIView.as_view(), name='materials'),
+    path('materials/<int:pk>/', MaterialDetailAPIView.as_view(), name='material'),
+    path('materials/update/<int:pk>/', MaterialUpdateAPIView.as_view(), name='material_update'),
+    path('materials/delete/<int:pk>/', MaterialDeleteAPIView.as_view(), name='material_delete'),
+
+    # test
+    path('add_test/', TestCreateAPIView.as_view(), name='add_test'),
+    path('tests/', TestListAPIView.as_view(), name='tests'),
+    path('tests/<int:pk>/', TestDetailAPIView.as_view(), name='test'),
+    path('tests/update/<int:pk>/', TestUpdateAPIView.as_view(), name='test_update'),
+    path('tests/delete/<int:pk>/', TestDeleteAPIView.as_view(), name='test_delete'),
 ]
