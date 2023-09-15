41bf1336a0cd6c3e56757e37742815b5b18c48f1
satel31 <blokhnina.tatiana@yandex.ru>
2023-09-15 22:07:26 +0300

    Updated serializers for material

--- apps/learning/serializers/material.py
+++ apps/learning/serializers/material.py
@@ -1,16 +1,13 @@
 from rest_framework import serializers
 
-from apps.learning.models import Material, Subscription, Test
+from apps.learning.models import Material, Test, Section
 from apps.learning.serializers.test import TestSerializer
 
 
 class MaterialSerializer(serializers.ModelSerializer):
     tests = TestSerializer(read_only=True, many=True)
-    subscription = serializers.SerializerMethodField()
     test_count = serializers.SerializerMethodField()
-
-    def get_subscription(self, material):
-        return Subscription.objects.filter(material=material).exists()
+    section = serializers.SlugRelatedField(slug_field='section_name', queryset=Section.objects.all())
 
     def get_test_count(self, material):
         return Test.objects.filter(material=material.pk).count()
