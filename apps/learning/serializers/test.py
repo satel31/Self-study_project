8ae3a2c4f22833cda85fd65bc1c92ed76f3e193e
satel31 <blokhnina.tatiana@yandex.ru>
2023-09-15 22:08:46 +0300

    Added serializers for test

--- apps/learning/serializers/test.py
+++ apps/learning/serializers/test.py
@@ -1,14 +1,24 @@
 from rest_framework import serializers
 
-from apps.learning.models import Test
+from apps.learning.models import Test, Question, UserAnswer
+from apps.learning.serializers.question import QuestionSerializer
 
 
 class TestSerializer(serializers.ModelSerializer):
-    # questions = QuestionSerializer
-    # try_count = serializers.SerializerMethodField()
+    question_count = serializers.SerializerMethodField()
+    right_answers = serializers.SerializerMethodField()
+    questions = QuestionSerializer(read_only=True, many=True)
 
-    #def get_try_count(self, test):
-        #return Try.objects.filter(test=test.pk).count()
+    def get_question_count(self, test):
+        return Question.objects.filter(test=test.pk).count()
+
+    def get_right_answers(self, test):
+        right_answers = 0
+        questions = Question.objects.filter(test=test.pk)
+        for question in questions:
+            if UserAnswer.objects.filter(question=question.pk).last().is_passed:
+                right_answers += 1
+        return right_answers
 
     class Meta:
         model = Test
