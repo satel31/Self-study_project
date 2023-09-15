90635be2df90343c2005bfeefef885f8bbc56354
satel31 <blokhnina.tatiana@yandex.ru>
2023-09-15 23:45:37 +0300

    Added permissions

--- apps/learning/views/section.py
+++ apps/learning/views/section.py
@@ -1,5 +1,7 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
+from apps.learning.permissions import ModeratorPermission
 from apps.learning.serializers.section import SectionSerializer
 from apps.learning.pagination import SectionPagination
 from apps.learning.models import Section
@@ -7,23 +9,28 @@ from apps.learning.models import Section
 
 class SectionCreateAPIView(generics.CreateAPIView):
     serializer_class = SectionSerializer
+    permission_classes = [IsAuthenticated, ModeratorPermission]
 
 
 class SectionListAPIView(generics.ListAPIView):
     serializer_class = SectionSerializer
     pagination_class = SectionPagination
     queryset = Section.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class SectionDetailAPIView(generics.RetrieveAPIView):
     serializer_class = SectionSerializer
     queryset = Section.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class SectionUpdateAPIView(generics.UpdateAPIView):
     serializer_class = SectionSerializer
     queryset = Section.objects.all()
+    permission_classes = [IsAuthenticated, ModeratorPermission]
 
 
 class SectionDeleteAPIView(generics.DestroyAPIView):
     queryset = Section.objects.all()
+    permission_classes = [IsAuthenticated, ModeratorPermission]
